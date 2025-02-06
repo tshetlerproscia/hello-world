92b2b730255b3c6ca08a22c65c61fa0f6c0188fb
Thomas Shetler <thomas.shetler@proscia.com>
2025-02-06 10:49:30 -0500

    Add new hostname route handler

--- src/main.go
+++ src/main.go
@@ -4,14 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
 
+func hostnameHandler(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Fprintln(w, "Hostname: ", hostname)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/os", hostnameHandler)
 	port := "8080"
 	log.Printf("Server is running on port %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
